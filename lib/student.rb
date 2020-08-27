0945904025ee35c94fac9a71d31cd73be5b34e73
Scott Wilson <scottwilson1020@gmail.com>
2020-08-27 21:17:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,10 @@ class Student
   
   attr_accessor :name, :grade
   
-  def initialize( name, grade)
-    
+def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
   end 
   
   def create_table()
